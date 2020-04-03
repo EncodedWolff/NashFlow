cee6615d4298b87dd90dc834aa1515e26312caa4
WolffJosephN <wolff.joseph.n@gmail.com>
2020-04-03 15:16:40 -0400

    Added Component folder to help with organization as well as creating the base of the Header component.

---
+++ nash-flow/src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+class Header extends React.Component {
+    constructor(){
+        super();
+    };
+
+    state = {};
+
+    render(){
+        return(
+            <div>
+                <h1>This is my header!</h1>
+            </div>
+        );
+    }
+}
